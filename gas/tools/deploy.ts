36a41b0df7952c79acd5216a575db39ed4b3063f
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-03-03 15:39:59 +0900

    chore: デプロイを使い回す

--- gas/tools/deploy.ts
+++ gas/tools/deploy.ts
@@ -1,14 +1,25 @@
 import { $ } from "bun";
 
 await $`clasp push -f`;
-const { stdout } = await $`clasp deploy`;
-const stdoutString = stdout.toString("utf-8");
+const deployments = await $`clasp deployments`.nothrow().text();
 
-const deployId = stdoutString.match(/- (\S+) @/)?.[1];
-if (!deployId) {
-  throw new Error("Failed to deploy");
+const mainDeployment = deployments.match(/- (\S+) @[0-9]+ - main/)?.[1];
+if (mainDeployment) {
+  console.log(`Redeploying: ${mainDeployment}`);
+  await $`clasp deploy -i ${mainDeployment} -d main`;
+  console.log();
+  console.log(`https://script.google.com/macros/s/${mainDeployment}/exec`);
+} else {
+  console.log("No main deployment found, deploying new version");
+  const { stdout } = await $`clasp deploy -d main`;
+  const stdoutString = stdout.toString("utf-8");
+
+  const deployId = stdoutString.match(/- (\S+) @/)?.[1];
+  if (!deployId) {
+    throw new Error("Failed to deploy");
+  }
+
+  console.log();
+  console.log(`Deployed: ${deployId}`);
+  console.log(`https://script.google.com/macros/s/${deployId}/exec`);
 }
-
-console.log();
-console.log(`Deployed: ${deployId}`);
-console.log(`https://script.google.com/macros/s/${deployId}/exec`);
