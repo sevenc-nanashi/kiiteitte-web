b5d327ae0e6e19d6aacbabc05f2bf00406dd312a
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-03-05 15:04:18 +0900

    chore: autostashをする

--- src/huggingFace.ts
+++ src/huggingFace.ts
@@ -48,7 +48,10 @@ export const updateHuggingFace = async () => {
 
   log.info("Updating Hugging Face repository");
   await Bun.spawn({ cmd: ["git", "reset", "--hard"], cwd: root }).exited;
-  await Bun.spawn({ cmd: ["git", "pull", "--rebase"], cwd: root }).exited;
+  await Bun.spawn({
+    cmd: ["git", "pull", "--autostash", "--rebase"],
+    cwd: root,
+  }).exited;
   log.info("Repository updated");
 
   const historyJsons = await Array.fromAsync(
